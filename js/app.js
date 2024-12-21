15ba3c59d02f57505207ac47659a4aa6c7ced65c
Earlskyyy <earlstevenpacaldo@gmail.com>
2024-12-22 02:56:52 +0800

    Kayat ta maam

--- js/app.js
+++ js/app.js
@@ -1,16 +1,30 @@
-let myimg = document.querySelector('#myimg');
-function reload() {
-fetch("https://randomfox.ca/floof/", {method:"GET"})
-  .then(response => response.json())
-  .then(result => {
-    myimg.src = result.image;
-  })
-  .catch(error => console.log('error', error));        
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+const fetchRecipes = async (query) => {
+  recipeContainer.innerHTML = "Fetching recipes...";
+  const data = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+  const response = await data.json();
+ 
+  recipeContainer.innerHTML = "";
+  response.meals.forEach(meal => {
+    const recipeDiv = document.createElement('div');
+    recipeDiv.classList.add('recipe');
+    recipeDiv.innerHTML = `
+        <img src="${meal.strMealThumb}">
+        <h3>${meal.strMeal}</h3>
+        <p>${meal.strArea}</p>
+        <p>${meal.strCategory}</p>
+    `
+    recipeContainer.appendChild(recipeDiv);
+  });
 }
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-     navigator.serviceWorker.register('sw.js').then( () => {
-      console.log('Service Worker Registered')
-     })
-   })
-  }
+
+
+searchBtn.addEventListener("click",  (e) => {
+  e.preventDefault();
+  const searchInput = searchBox.value.trim();
+    fetchRecipes(searchInput);
+
+    });
